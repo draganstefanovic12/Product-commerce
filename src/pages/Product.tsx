d0f75bcf9ecddef21118dc9eb90edd41b11c1c07
draganstefanovic12 <stefanovicdragan112@gmail.com>
2022-10-01 03:42:10 +0200

    working on image layout

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,19 +1,39 @@
-import { useQuery, UseQueryResult } from "react-query";
 import { useParams } from "react-router-dom";
 import { getProduct } from "../api/productApi";
 import { Product as ProductPage } from "../features/products/types";
+import { useQuery, UseQueryResult } from "react-query";
+import Container from "../components/Container";
+import { useState } from "react";
 
 const Product = () => {
   const { id } = useParams();
   const { isLoading, data: product } = useQuery(["product"], () => {
     return getProduct(id);
   }) as UseQueryResult<ProductPage>;
+  const [number, setNumber] = useState(0);
 
   if (isLoading) {
     return <p>Loading...</p>;
   }
 
-  return <div>{product?.name}</div>;
+  const handleNextImage = () => {
+    setNumber((currNum) => currNum + 1);
+  };
+
+  const handlePreviousImage = () => {
+    setNumber((currNum) => currNum - 1);
+  };
+
+  return (
+    <Container>
+      <div>
+        <img
+          src={`http://localhost:5006${product?.images![number]}`}
+          alt="img"
+        />
+      </div>
+    </Container>
+  );
 };
 
 export default Product;
